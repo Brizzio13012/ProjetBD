f2f5d87577f987839b55efeba29a3eab9df8cbf0
Brizzio13012 <fa.pont@gmail.com>
2018-12-09 19:19:26 +0100

    xml

--- Interface/src/TestAjout.java
+++ Interface/src/TestAjout.java
@@ -6,7 +6,7 @@ public class TestAjout {
 
     public static void main(String[] args) {
         UtilisateurDAO daoUser= new UtilisateurDAO(SConnection.getInstance());
-        Utilisateur user = new Utilisateur("Pont", "Fabrice", "leducqt@gmail.com", "21 BD Gambetta");
+        Utilisateur user = new Utilisateur("Pont", "Fabrice", "leducqt@gmail", "21 BD Gambetta");
         daoUser.create(user);
     }
 }
